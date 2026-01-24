e0cb2e285275ccd62152a6a316d2670ff34b591a
Amir-Ramez <ID+Amir-Ramez@users.noreply.github.com>
2026-01-24 18:46:29 +0200

    Solve 07_setters

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -3,6 +3,8 @@
 //   Even better, extract that logic and reuse it in both places. You can use
 //   private functions or private static methods for that.
 
+use common::valid_title;
+
 pub struct Ticket {
     title: String,
     description: String,
@@ -11,21 +13,9 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        check_title(&title);
+        check_description(&description);
+        check_status(&status);
 
         Ticket {
             title,
@@ -44,6 +34,45 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        check_title(&new_title);
+        self.title = new_title
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        check_description(&new_description);
+        self.description = new_description
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        check_status(&new_status);
+        self.status = new_status
+    }
+}
+
+fn check_title(new_title: &String) {
+    if new_title.is_empty() {
+        panic!("Title cannot be empty");
+    }
+    if new_title.len() > 50 {
+        panic!("Title cannot be longer than 50 bytes");
+    }
+}
+
+fn check_description(new_description: &String) {
+    if new_description.is_empty() {
+        panic!("Description cannot be empty");
+    }
+    if new_description.len() > 500 {
+        panic!("Description cannot be longer than 500 bytes");
+    }
+}
+
+fn check_status(new_status: &String) {
+    if new_status != "To-Do" && new_status != "In Progress" && new_status != "Done" {
+        panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
     }
 }
 
